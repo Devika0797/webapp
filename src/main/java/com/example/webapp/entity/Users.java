1a5ac944e68cb73f762a63657cb53d50ec7c5d7c
Devika0797 <devika-97@mail.ru>
2022-03-27 21:53:14 +0300

    сделала набросок личного кабинета. на неделе подключу базу данных, добавлю пройденные тесты

---
+++ src/main/java/com/example/webapp/entity/Users.java
@@ -0,0 +1,59 @@
+package com.example.webapp.entity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class Users {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private int user_id;
+
+    @Column(name = "username")
+    private String username;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "points")
+    private int points;
+
+
+
+    public int getUser_id() {
+        return user_id;
+    }
+
+    public void setUser_id(int user_id) {
+        this.user_id = user_id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+
+    public int getPoints() {
+        return points;
+    }
+
+    public void setPoints(int points) {
+        this.points = points;
+    }
+
+
+}
